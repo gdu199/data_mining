15c13fcc02b689b6866ae1c8dda4b7b0fb279917
Dmitriy <gdu199@mail.ru>
2020-12-28 11:55:54 +0300

    Insta забанила меня. Успел найти короткие связи. Конечно кривенько, но хотя бы работает

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+from scrapy.crawler import CrawlerProcess
+from scrapy.settings import Settings
+
+from gb_parse.spiders.instagram import InstagramSpider
+
+import dotenv
+
+# from gb_parse import settings
+dotenv.load_dotenv('.env')
+if __name__ == '__main__':
+    crawl_settings = Settings()
+    crawl_settings.setmodule('gb_parse.settings')
+    # crawl_settings.setmodule(settings)
+    crawl_proc = CrawlerProcess(settings=crawl_settings)
+    #так создается объект паук и передаются аргументы в виде *kwargs
+    crawl_proc.crawl(InstagramSpider, login=os.getenv('LOGIN'), enc_password=os.getenv('PASSWORD'), user1='eshtokin.o', user2 = 'marina.nenahova')
+    crawl_proc.start()
